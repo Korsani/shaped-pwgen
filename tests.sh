6a955d4a9c27298b0348dc9b01e6d2557293f10b
Gabriel Guillon <gabriel@guillon.world>
2022-02-08 10:34:11 +0100

    Added some tests

---
+++ tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+function succeed() {
+	echo 'Test ok'
+}
+spwgen -8 -2			>	/dev/null			|| succeed
+spwgen					>	/dev/null 			&& succeed
+spwgen -k 23			>	/dev/null			|| succeed
+spwgen -k 				>	/dev/null			&& succeed
+spwgen -a triangle 		>	/dev/null			&& succeed
+spwgen -a azerty 		>	/dev/null	2>&1	|| succeed
